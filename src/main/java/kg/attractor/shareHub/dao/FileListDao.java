a2dd2f778beda4f936a1d17864ad44053ffef165
Sanat Bakytbekova <sanatbakytbekova@gmail.com>
2023-08-26 15:43:49 +0600

    edieted table names

--- src/main/java/kg/attractor/shareHub/dao/FileListDao.java
+++ src/main/java/kg/attractor/shareHub/dao/FileListDao.java
@@ -16,26 +16,26 @@ public class FileListDao {
     private final JdbcTemplate jdbcTemplate;
 
     public void save(File profileImage) {
-        String sql = "insert into profile_images(userid,filename,status) " +
+        String sql = "insert into files(userid,filename,status) " +
                 "values  ( ? , ? , ? )";
         jdbcTemplate.update(sql, profileImage.getUserId(), profileImage.getFileName(),profileImage.getStatus());
     }
 
     public File getImageById(int imageId) {
-        String sql = "select * from profile_images where id = ?";
+        String sql = "select * from files where id = ?";
         return DataAccessUtils.singleResult(jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(File.class), imageId));
     }
 
     public List<File> getImageByUserId(int userId) {
-        String sql = "select * from profile_images where userId = ?";
+        String sql = "select * from files where userId = ?";
         return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(File.class), userId);
     }
     public List<File> getAllFiles(){
-        String sql = "select * from profile_images ";
+        String sql = "select * from files ";
         return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(File.class));
     }
     public void delete(int id) {
-        String sql = "DELETE FROM profile_images WHERE id = ?";
+        String sql = "DELETE FROM files WHERE id = ?";
         jdbcTemplate.update(sql, id);
     }
 

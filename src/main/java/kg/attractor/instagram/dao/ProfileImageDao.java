f39763c0b33b8c6bcddabe500d50cf2fb4696af7
Sanat Bakytbekova <sanatbakytbekova@gmail.com>
2023-08-26 10:46:15 +0600

    Initial commit

---
+++ src/main/java/kg/attractor/instagram/dao/ProfileImageDao.java
@@ -0,0 +1,34 @@
+package kg.attractor.instagram.dao;
+
+
+import kg.attractor.instagram.model.ProfileImage;
+import lombok.RequiredArgsConstructor;
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.dao.support.DataAccessUtils;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class ProfileImageDao {
+    private final JdbcTemplate jdbcTemplate;
+
+    public void save(ProfileImage profileImage) {
+        String sql = "insert into profile_images(userid,filename) " +
+                "values  ( ? , ? )";
+        jdbcTemplate.update(sql, profileImage.getUserId(), profileImage.getFileName());
+    }
+
+    public ProfileImage getImageById(int imageId) {
+        String sql = "select * from profile_images where id = ?";
+        return DataAccessUtils.singleResult(jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(ProfileImage.class), imageId));
+    }
+
+    public List<ProfileImage> getImageByUserId(int userId) {
+        String sql = "select * from profile_images where userId = ?";
+        return jdbcTemplate.query(sql, new BeanPropertyRowMapper<>(ProfileImage.class), userId);
+    }
+}
